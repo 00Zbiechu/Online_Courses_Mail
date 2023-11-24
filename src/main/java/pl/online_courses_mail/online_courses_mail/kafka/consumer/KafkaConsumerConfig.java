b93834af4f1d0bb77a38f711db0cb1ba1c25a3d2
Windows <00Zbiewski@gmail.com>
2023-11-24 17:54:13 +0100

    Przeniesienie danych do application.properties

--- src/main/java/pl/online_courses_mail/online_courses_mail/kafka/consumer/KafkaConsumerConfig.java
+++ src/main/java/pl/online_courses_mail/online_courses_mail/kafka/consumer/KafkaConsumerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -14,13 +15,17 @@ import pl.online_courses_mail.online_courses_mail.dto.UsernameAndMailDTO;
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
+
+    @Value("${online-courses.kafka.topics.email.trusted-packages}")
+    private String trustedPackages;
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServer;
 
-    public Map<String, Object> consumerConfig() {
+    private Map<String, Object> consumerConfig() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -30,10 +35,9 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConsumerFactory<String, UsernameAndMailDTO> registrationConsumerFactory() {
-
         JsonDeserializer<UsernameAndMailDTO> deserializer = new JsonDeserializer<>(UsernameAndMailDTO.class);
         deserializer.setRemoveTypeHeaders(false);
-        deserializer.addTrustedPackages("*");
+        deserializer.addTrustedPackages(trustedPackages);
         deserializer.setUseTypeMapperForKey(true);
 
         return new DefaultKafkaConsumerFactory<>(
--- src/main/java/pl/online_courses_mail/online_courses_mail/kafka/listener/KafkaRegistrationListener.java
+++ src/main/java/pl/online_courses_mail/online_courses_mail/kafka/listener/KafkaRegistrationListener.java
@@ -1,6 +1,7 @@
 package pl.online_courses_mail.online_courses_mail.kafka.listener;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import pl.online_courses_mail.online_courses_mail.dto.UsernameAndMailDTO;
 import pl.online_courses_mail.online_courses_mail.service.EmailSenderService;
@@ -11,11 +12,7 @@ public class KafkaRegistrationListener {
 
     private final EmailSenderService emailSenderService;
 
-    @org.springframework.kafka.annotation.KafkaListener(
-            topics = "registration",
-            groupId = "registration",
-            containerFactory = "registrationFactory"
-    )
+    @KafkaListener(topics = "${online-courses.kafka.topics.email.name}", groupId = "${online-courses.kafka.topics.email.group}", containerFactory = "registrationFactory")
     void sendMail(UsernameAndMailDTO usernameAndMailDTO) {
         emailSenderService.sendRegistrationEmail(usernameAndMailDTO.getMail(), usernameAndMailDTO.getUsername());
     }
